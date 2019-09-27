c83e93e816d4a4bb5f008e97d95061c1d870ce49
Sercan YILDIZ <ysercanz@gmail.com>
2019-09-28 00:22:50 +0200

    Fix: Add the build image cmd line :d

--- builder.sh
+++ builder.sh
@@ -5,5 +5,6 @@ PKG=action-deployer-php
 VERSION=latest
 
 docker login $HUB --username $BUILD_USERNAME --password $BUILD_TOKEN
+docker build -t $PKG .
 docker tag $PKG $HUB/$BUILD_USERNAME/$PKG/$PKG:$VERSION
 docker push $HUB/$BUILD_USERNAME/$PKG/$PKG:$VERSION
